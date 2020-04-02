6ef7658f5cc5657f9a83c0cddd76ede977bd6703
oznekpark <kgog.33@gmail.com>
2020-04-02 13:47:53 +0900

    自動更新機能の実装

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message) {
     if (message.text && message.image) {
       var html = 
-        `<div class="chat-main__messages_message">
+        `<div class="chat-main__messages__message" data-message-id=${message.id}>
           <div class="chat-main__messages__message__upper-info">
             <div class="chat-main__messages__message__upper-info__talker">
               ${message.user_name}
@@ -20,7 +20,7 @@ $(function(){
         </div>`
     } else if (message.text) {
       var html = 
-        `<div class="chat-main__messages_message">
+        `<div class="chat-main__messages__message" data-message-id=${message.id}>
           <div class="chat-main__messages__message__upper-info">
             <div class="chat-main__messages__message__upper-info__talker">
               ${message.user_name}
@@ -37,43 +37,78 @@ $(function(){
         </div>`
     } else if (message.image) {
       var html = 
-        `<div class="chat-main__messages_message">
-          <div class="chat-main__messages__message__upper-info">
-            <div class="chat-main__messages__message__upper-info__talker">
-              ${message.user_name}
-            </div>
-            <div class="chat-main__messages__message__upper-info__data">
-              ${message.created_at}
-            </div>
+        `<div class="chat-main__messages__message" data-message-id=${message.id}>
+        <div class="chat-main__messages__message__upper-info">
+          <div class="chat-main__messages__message__upper-info__talker">
+            ${message.user_name}
           </div>
-          <div class="chat-main__messages__message__lower-info">
-            <img src ="${message.image}" class="chat-main__messages__message__lower-info__image">
+          <div class="chat-main__messages__message__upper-info__data">
+            ${message.created_at}
           </div>
-        </div>`
+        </div>
+        <div class="chat-main__messages__message__lower-info">
+          <img src ="${message.image}" class="chat-main__messages__message__lower-info__image">
+        </div>
+      </div>`
       };
     return html;
   }
-  $("#new_message").on("submit", function(e){
+  $('#new_message').on('submit', function(e){
   e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr("action");
+    var url = $(this).attr('action')
     $.ajax({
       url: url,
       type: "POST",
       data: formData,
-      dataType: "json",
+      dataType: 'json',
       processData: false,
       contentType: false
     })
     .done(function(data){
       var html = buildHTML(data);
       $('.chat-main__messages').append(html);
-      $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
       $('form')[0].reset();
-      $('.chat-main__form__form-parents__send-btn').prop('disabled', false);
+      $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
+      // $('.chat-main__form__form-parents__send-btn').prop('disabled', false);
     })
     .fail(function(){
       alert("メッセージ送信に失敗しました");
+    })
+    .always(function(){	
+      $("input").prop("disabled", false);	
     });
   });
+  
+  // 自動更新
+  var reloadMessages = function(){
+    var last_message_id = $('.chat-main__messages__message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: "get",
+      dataType: "json",
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      console.log(messages)
+      if (messages.length !== 0){
+        //追加するHTMLの入れ物を作る
+        var insertHTML = "";
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.chat-main__messages').append(insertHTML);
+        $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+      alert("error");
+    });
+  }
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    console.log(document.location.href)
+    setInterval(reloadMessages, 7000);
+  }
 });
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,63 +1,63 @@
 $(function(){
-
-  var search_list = $("#user-search-result");
-
   function addUser(user){
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${user.name}</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
-                </div>`;
-    search_list.append(html);
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
+      </div>
+    `;
+    $("#user-search-result").append(html);
   } 
-
   function addNouser(){
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">ユーザーが見つかりません</p>
-                </div>`;
-    search_list.append(html);
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>
+    `;
+    $("#user-search-result").append(html);
   }
-
   function addDeleteUser(name, id){
-    let html = `<div class="chat-group-user clearfix" id="${id}">
-                  <input name='group[user_ids][]' type='hidden' value="${id}">
-                  <p class="chat-group-user__name">${name}</p>
-                  <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
-                </div>`;
+    let html = `
+      <div class="chat-group-user clearfix" id="${id}">
+        <p class="chat-group-user__name">${name}</p>
+        <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+      </div>
+    `;
     $(".js-add-user").append(html)
   }
-
   function addMember(userId){
-    var html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
     $(`#${userId}`).append(html);
   }
-
-  $("#user-search-field").on("keyup", function(){
-    var input = $('#user-search-field').val();
+  $("#user-search-field").on("keyup", function() {
+    let input = $("#user-search-field").val();
     $.ajax({
       type: "GET",
       url: "/users",
-      data: { keyword: input},
-      dataType: 'json'
+      data: { keyword: input },
+      dataType: "json"
     })
-    .done(function(users){
-      search_list.empty();
-      if (users.length !== 0){
-        users.forEach(function(user){
-          addUser(user);
-        });
-      } else if (input.length == 0){
-        return false;
-      } else {
-        addNouser();
-      }
-    })
-    .fail(function(){
-      alert("ユーザー検索に失敗しました");
-    });
+      .done(function(users) {
+        $("#user-search-result").empty();
+
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            addUser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoUser();
+        }
+      })
+      .fail(function() {
+        alert("通信エラーです。ユーザーが表示できません。");
+      });
   });
-  $(document).on("click", ".chat-group-user__btn--add",function(){
-    var userName = $(this).attr("data-user-name");
-    var userId = $(this).attr("data-user-id");
+  $(document).on("click", ".chat-group-user__btn--add", function() {
+    console.log
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
     $(this)
       .parent()
       .remove();
